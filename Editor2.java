1fb09364fc4c655cd2a90b11c6539f43cb047266
Zowde <157274222+Zowde@users.noreply.github.com>
2024-02-28 14:10:01 +0200

    Update Editor2.java

--- Editor2.java
+++ Editor2.java
@@ -11,8 +11,8 @@ public class Editor2 {
 
 	public static void main (String[] args){
 		String fileName = args[0];
-		int width = Integer.parseInt(args[1]);
-		int height = Integer.parseInt(args[2]);
+		int width  =Integer.parseInt(args[1]);
+		int height =Integer.parseInt(args[2]);
 		Color[][] imageIn = Runigram.read(fileName);
 		Color[][] scaledImage = Runigram.scaled(imageIn, width, height);
 		// Displays the source image
