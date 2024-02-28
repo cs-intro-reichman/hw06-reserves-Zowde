52fe57b1354217ca4ab7e046083efe68ada12f28
Zowde <157274222+Zowde@users.noreply.github.com>
2024-02-28 14:10:25 +0200

    Update Editor3.java

--- Editor3.java
+++ Editor3.java
@@ -13,12 +13,14 @@ import java.awt.Color;
 public class Editor3 {
 
 	public static void main (String[] args) {
-		String source = args[0];
+		String source =args[0];
 		String target = args[1];
-		int n = Integer.parseInt(args[2]);
+		int n =500; //Integer.parseInt(args[2]);
 		Color[][] sourceImage = Runigram.read(source);
 		Color[][] targetImage = Runigram.read(target);
 		Runigram.setCanvas(sourceImage);
 		Runigram.morph(sourceImage, targetImage, n);
+		}
 	}
-}
+
+	
