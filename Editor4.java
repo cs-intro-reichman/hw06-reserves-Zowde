b484dac1edf562c78dad9680d1290b85a60c09cd
Zowde <157274222+Zowde@users.noreply.github.com>
2024-02-29 13:32:32 +0200

    Update Editor4.java

--- Editor4.java
+++ Editor4.java
@@ -3,9 +3,12 @@ public class Editor4 {
 
     public static void main (String[] args) {
 		String source = args[0];
-		int n =Integer.parseInt(args[2]);
+	       int n =Integer.parseInt(args[2]);
 		Color[][] sourceImage = Runigram.read(source);
+		Color[][] sourceImage2 = Runigram.grayScaled(sourceImage);//gray form of the image
 		Runigram.setCanvas(sourceImage);
-		Runigram.grayScaled(sourceImage, n);
+		Runigram.morph(sourceImage,sourceImage2, n);
+
+	    
 		}
 }
